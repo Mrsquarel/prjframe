acd7f018d83e6b65e4b0dd6f01a85ad3bb97c6c0
Mrsquarel <amine.gharsallah@yahoo.fr>
2025-04-06 14:58:49 +0100

    test patient

--- prjframe/Form4.Designer.cs
+++ prjframe/Form4.Designer.cs
@@ -320,7 +320,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(516, 109);
+            this.label7.Location = new System.Drawing.Point(516, 104);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(201, 20);
             this.label7.TabIndex = 10;
